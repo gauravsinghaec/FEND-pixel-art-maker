e9684d126d614fbf5286ce27969bc5f8d921e72e
Gaurav Singh <gauravsinghaec@gmail.com>
2018-03-25 12:43:11 +0530

    Update font style and Add download button

--- designs.js
+++ designs.js
@@ -9,22 +9,27 @@ var subBtn = $('#submit');
 // var resetBtn = $('#reset');
 var x = "<tr class='canvasRow'></tr>";
 var y = "<td class='canvasCol'></td>";
-
 function makeGrid() {
 // Your code goes here!
 	$(".canvasRow:last").append(y);
 	$(".canvasCol:last").on('click',function(e){
 		selecteColor = $(color).val();
-		$(this).css("background-color",selecteColor);		
+		preColor = $(this).css("background-color");
+		if(preColor == "rgb(255, 255, 255)" 
+			|| preColor == "rgba(0, 0, 0, 0)"){
+			$(this).css("background-color",selecteColor);		
+		}else{
+			$(this).css("background-color","rgb(255, 255, 255)");		
+		}
 	})
 }
 
 $("#inputHeight, #inputWeight").on('change',function(evt){
 	console.log($(this).val());	
-	if($(this).val() >25){
+	if($(this).val() >30){
 		$(subBtn).prop('disabled','true');
 		$("#error").removeAttr('hidden');
-		$("#error").text("Max allowable height/width value is 25.");
+		$("#error").text("Max allowable height/width value is 30.");
 		$("#error").css("color","red");
 	}else{
 		$("#error").attr("hidden","true");
@@ -44,11 +49,31 @@ $('#submit').on('click',function(evt){
 			makeGrid();
 		}
 	}
-	console.log(col,row,$(color).val());
+	$('table').css("background-color", "white");
+	$('#save').removeAttr('hidden');
 });
-/*
+
 $('#reset').on('click',function(evt){
+	$("#save").attr("hidden","true");
 	$(".canvasRow").remove();
 	$(".canvasCol").remove();
 });
-*/
+
+var imgLink = $("<a id='myimage'>");
+
+$('#save').on('click',function(evt){
+	html2canvas(document.querySelector("#table"))
+	.then(canvas => {
+    document.body.appendChild(canvas)
+    var img = canvas.toDataURL("image/png")
+    			.replace("image/png", "image/octet-stream");
+	$(imgLink)
+	    .attr("href", img)
+	    .attr("download", "pixel_art.png")
+	    .appendTo(canvas)
+	    .text("Click Me");
+	document.getElementById("myimage")
+		.click();
+	$(canvas).remove();    
+	});
+});
